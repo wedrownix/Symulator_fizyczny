d7ae27e4defaef433d3cbdcbd29c304ad3d25386
Jozef Kozluk <josefkozluk@gmail.com>
2026-05-13 11:40:48 +0200

    Testuję początki pygame

---
+++ Nauka_Pygame.py
@@ -0,0 +1,15 @@
+import pygame
+pygame.init()
+
+#TWORZENIE OKNA, nasz win
+win = pygame.display.set_mode((800,600))
+
+pygame.display.set_caption("Nauka_Pygame")
+screen = pygame.display.set_mode((800,600))
+
+
+#PARAMETRY PIERWSZEJ POSTACI
+x = 50
+y = 50
+width = 400
+height = 400heigh = 60
