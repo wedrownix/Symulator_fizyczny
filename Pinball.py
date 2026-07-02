746a301e1c61f24ed840f2885f654feb853e1a3b
Jozef Kozluk <josefkozluk@gmail.com>
2026-07-02 10:12:25 +0200

    VEC2 z anatacjami i nową metodą

--- Pinball.py
+++ Pinball.py
@@ -22,3 +22,53 @@ def cX(x):
 def cY(y):
     return screen_height - y *cScale
 
+#%% VECTOR
+import math
+
+
+class Vec2:
+    def __init__(self, x: float = 0.0, y: float = 0.0):
+        self.x = x
+        self.y = y
+
+    def set(self, v):
+        self.x = v.x
+        self.y = v.y
+
+    def clone(self):
+        return Vec2(self.x, self.y)
+
+    def add(self, v, s: float = 1.0):
+        self.x += v.x * s
+        self.y += v.y * s
+        return self
+
+    def addVectors(self, a, b):
+        self.x = a.x + b.x
+        self.y = a.y + b.y
+        return self
+
+    def subtract(self, v, s: float = 1.0):
+        self.x -= v.x * s
+        self.y -= v.y * s
+        return self
+
+    def subtractVectors(self, a, b):
+        self.x = a.x - b.x
+        self.y = a.y - b.y
+        return self
+
+    def length(self):
+        return math.sqrt(self.x * self.x + self.y * self.y)
+
+    def scale(self, s: float):
+        self.x *= s
+        self.y *= s
+        return self
+
+    def dot(self, v):
+        return self.x * v.x + self.y * v.y
+
+    def perp(self):
+        """Zwraca wektor prostopadły (-y, x)."""
+        return Vec2(-self.y, self.x)
