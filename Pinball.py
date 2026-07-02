f13f3c8f206d62fb4480ac4a0bce8bf551281d20
Jozef Kozluk <josefkozluk@gmail.com>
2026-07-02 10:20:27 +0200

    klasa obstacle

--- Pinball.py
+++ Pinball.py
@@ -103,3 +103,8 @@ class Ball:
         self.vel.add(gravity, dt)
         self.pos.add(self.vel, dt)
 
+class Obstacle:
+    def __init__(self, radius, pos, pushVel):
+        self.radius = radius
+        self.pos = pos.clone()
+        self.pushVel = pushVel
