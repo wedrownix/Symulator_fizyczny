a518380a5ecd3cfb8bafd84bf23b6d882d743086
Jozef Kozluk <josefkozluk@gmail.com>
2026-09-06 10:00:08 +0200

    Tworzę nowy plik oparty na więzach. Zaczynam od podstawowych ustawień

---
+++ Constraints.py
@@ -0,0 +1,77 @@
+import math
+import random
+import pygame
+pygame.init()
+from typing import List
+
+#%%TWORZENIE OKNA,
+screen_width = 1000
+screen_height = 700
+win = pygame.display.set_mode((screen_width,screen_height))
+pygame.display.set_caption("Nauka_Symulatora")
+clock = pygame.time.Clock()
+
+
+simMinWidth = 20 #definiuje minimalną odległość obserwowaną na ekranie
+cScale = min(screen_width,screen_height)/simMinWidth
+simWidth = screen_width/cScale
+simHeight = screen_height/cScale
+
+def cX(x):
+    return x *cScale
+def cY(y):
+    return screen_height - y *cScale
+
+#%% VECTOR
+import math
+
+
+class Vec2:
+    def __init__(self, x: float = 0.0, y: float = 0.0):
+        self.x = x
+        self.y = y
+
+    def set(self, v):
+        self.x = v.x
+        self.y = v.y
+
+    def clone(self):
+        return Vec2(self.x, self.y)
+
+    def add(self, v, s: float = 1.0):
+        self.x += v.x * s
+        self.y += v.y * s
+        return self
+
+    def addVectors(self, a, b):
+        self.x = a.x + b.x
+        self.y = a.y + b.y
+        return self
+
+    def subtract(self, v, s: float = 1.0):
+        self.x -= v.x * s
+        self.y -= v.y * s
+        return self
+
+    def subtractVectors(self, a, b):
+        self.x = a.x - b.x
+        self.y = a.y - b.y
+        return self
+
+    def length(self):
+        return math.sqrt(self.x * self.x + self.y * self.y)
+
+    def scale(self, s: float):
+        self.x *= s
+        self.y *= s
+        return self
+
+    def dot(self, v):
+        return self.x * v.x + self.y * v.y
+
+    def perp(self):
+        """Zwraca wektor prostopadły (-y, x)."""
+        return Vec2(-self.y, self.x)
+
+
+
