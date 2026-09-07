a3b3b8cbd13065212879ffbc6ff8f29676d22f21
Jozef Kozluk <josefkozluk@gmail.com>
2026-09-07 15:18:06 +0200

    Restart symualcji

--- Constraints.py
+++ Constraints.py
@@ -169,6 +169,10 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_r:  # restart symulacji
+                setup_scene()
+
 
     simulate()
     draw()
